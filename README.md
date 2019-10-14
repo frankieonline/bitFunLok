c319eda18279123a9662d8ce4716ac6a34e5c038
frankieonline <frankie.wwl@gmail.com>
2019-10-14 08:28:28 +0800

    Added

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# bitFunLok
- MOSMPS MI bit Fun Lok
+# bit Fun 樂
+
+![](icon.png)
+
+## License
+
+MIT
+Frankie Wong @ Tomato 3D Studio 蕃茄工作室
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+bitFunLok=github:frankieonline/bitFunLok
+```
---
+++ bitFunLok.ts
@@ -0,0 +1,19 @@
+//% weight=0 color=#CCff33 icon="\uf001" block="bit Fun 樂"
+namespace bitFunLok {
+    /** Return the input of the Foil Paper */
+    //% block="paino status (P2, P8, P1)"
+    //% blockId="getPainoStatus"
+    //% weight=1
+    export function getPainoStatus():number {
+        let foil2 = pins.digitalReadPin(DigitalPin.P2);
+        let foil1 = pins.digitalReadPin(DigitalPin.P8);
+        let foil0 = pins.digitalReadPin(DigitalPin.P1);
+
+        foil2 = ((foil2 == PulseValue.High) ? (1) : (0));
+        foil1 = ((foil1 == PulseValue.High) ? (1) : (0));
+        foil0 = ((foil0 == PulseValue.High) ? (1) : (0));
+
+        let status = foil2 * 4 + foil1 * 2 + foil0;
+        return status;
+    }
+}
