7b1edf7cf70716da15483571a3e80fa2e8edc0ed
Frankie Wong <frankie.wwl@gmail.com>
2019-10-14 18:05:51 +0800

    updated

--- bitFunLok.ts
+++ bitFunLok.ts
@@ -5,9 +5,12 @@ namespace bitFunLok {
     //% blockId="getPainoStatus"
     //% weight=1
     export function getPainoStatus():number {
-        let foil2 = ((pins.digitalReadPin(DigitalPin.P2) == PulseValue.High) ? (1) : (0));
-        let foil1 = ((pins.digitalReadPin(DigitalPin.P8) == PulseValue.High) ? (1) : (0));
-        let foil0 = ((pins.digitalReadPin(DigitalPin.P1) == PulseValue.High) ? (1) : (0));
+        let foil2: number;
+        foil2 = ((pins.digitalReadPin(DigitalPin.P2) == PulseValue.High) ? (1) : (0));
+        let foil1: number;
+        foil1 = ((pins.digitalReadPin(DigitalPin.P8) == PulseValue.High) ? (1) : (0));
+        let foil0: number;
+        foil0 = ((pins.digitalReadPin(DigitalPin.P1) == PulseValue.High) ? (1) : (0));
 
         let status = foil2 * 4 + foil1 * 2 + foil0;
         return status;
